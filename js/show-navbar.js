5f40c18a7761a64ca6f902f7dedc5375c45f68fa
Alvaro Suarez Perez <suarezperezalvaro@gmail.com>
2015-10-15 16:00:51 +0200

    changes and blue logo

--- js/show-navbar.js
+++ js/show-navbar.js
@@ -8,7 +8,7 @@ $(window).scroll(function() {
     $('nav').css('top','-200px');
     $('#go-to-top').css('bottom','-100px');
   }
-  if (distanceToTop > 150){
+  if (distanceToTop > 10){
     $('nav').css('top','0px');
     $('nav').addClass('show');
     $('#go-to-top').css('bottom','0px');

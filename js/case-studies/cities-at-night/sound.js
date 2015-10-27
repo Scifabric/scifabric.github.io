8d234e5fe35b879543678fbe832bec81ef85b66b
Daniel Lombraña González <teleyinex@gmail.com>
2015-10-27 12:58:31 +0100

    Add audio to case study.

---
+++ js/case-studies/cities-at-night/sound.js
@@ -0,0 +1,82 @@
+// create WebAudio API context
+var context = new AudioContext()
+
+    // Create lineOut
+var lineOut = new WebAudiox.LineOut(context)
+
+// load a sound and play it immediatly
+WebAudiox.loadBuffer(context, '/assets/snd/loop.mp3', function(buffer){
+        // init AudioBufferSourceNode
+        var source  = context.createBufferSource();
+        source.buffer   = buffer;
+        source.connect(lineOut.destination);
+        // start the sound now
+        source.start(0);
+        });
+
+// load a sound and play it immediatly
+WebAudiox.loadBuffer(context, '/assets/snd/3.mp3', function(buffer){
+        // init AudioBufferSourceNode
+        var source  = context.createBufferSource();
+        source.buffer   = buffer;
+        source.connect(lineOut.destination)
+        var waypoint = new Waypoint({
+                element: document.getElementById('3'),
+                handler: function(direction) {
+                    source.start(0);
+                    console.log("3 step");},
+                offset: 120 
+            });
+        });
+
+
+// load a sound and play it immediatly
+WebAudiox.loadBuffer(context, '/assets/snd/2.mp3', function(buffer){
+        // init AudioBufferSourceNode
+        var source  = context.createBufferSource();
+        source.buffer   = buffer
+        source.connect(lineOut.destination)
+        var waypoint = new Waypoint({
+                element: document.getElementById('2'),
+                handler: function(direction) {
+                    source.start(0);
+                    console.log(direction);
+                    console.log('2 step');
+                                },
+                offset: 120 
+            });
+        });
+
+// load a sound and play it immediatly
+WebAudiox.loadBuffer(context, '/assets/snd/1.mp3', function(buffer){
+        // init AudioBufferSourceNode
+        var source  = context.createBufferSource();
+        source.buffer   = buffer
+        source.connect(lineOut.destination)
+        var waypoint = new Waypoint({
+                element: document.getElementById('1'),
+                handler: function(direction) {
+                    source.start(0);
+                    console.log(direction);
+                    console.log('1 step');
+                                },
+                offset: 120 
+            });
+        });
+
+// load a sound and play it immediatly
+WebAudiox.loadBuffer(context, '/assets/snd/0.ogg', function(buffer){
+        // init AudioBufferSourceNode
+        var source  = context.createBufferSource();
+        source.buffer   = buffer
+        source.connect(lineOut.destination)
+        var waypoint = new Waypoint({
+                element: document.getElementById('liftoff'),
+                handler: function(direction) {
+                    source.start();
+                    console.log(direction);
+                    console.log('liftoff');
+                                },
+                offset: 120 
+            });
+        });

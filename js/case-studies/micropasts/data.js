3ee2d7512891d90c56e51fef51ca188479ba3138
Daniel Lombraña González <teleyinex@gmail.com>
2016-03-07 14:50:48 +0100

    Update data in real time.
    NOTE: I've to change the Task completed to Submitted answers because the
    numbers in the server were different.

---
+++ js/case-studies/micropasts/data.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    var url = "http://crowdsourced.micropasts.org/api/globalstats";
+    
+    $.ajax({
+      dataType: "json",
+      url: url,
+      success: success
+    });
+    
+    function success(data){
+        var n_users = Math.floor(data['n_users']/100)*100;
+        $("#engagedUsers").html(n_users);
+
+        var taskCompleted = Math.floor(data['n_task_runs']/100)*100;
+        $("#taskCompleted").html(taskCompleted);
+
+        var n_projects = Math.floor(data['n_projects']/10)*10;
+        $("#n_projects").html(n_projects);
+    }
+});
+

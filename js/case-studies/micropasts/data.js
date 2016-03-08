ab6ee12ab22b7425ae97d497b8c8b26aee0c6eb3
Daniel Lombraña González <teleyinex@gmail.com>
2016-03-08 12:14:41 +0100

    Use toLocaleString to show the comma or dot for thousands.

--- js/case-studies/micropasts/data.js
+++ js/case-studies/micropasts/data.js
@@ -10,13 +10,13 @@ $(document).ready(function(){
     
     function success(data){
         var n_users = Math.floor(data['n_users']/100)*100;
-        $("#engagedUsers").html(n_users);
+        $("#engagedUsers").html(n_users.toLocaleString());
 
         var taskCompleted = Math.floor(data['n_task_runs']/100)*100;
-        $("#taskCompleted").html(taskCompleted);
+        $("#taskCompleted").html(taskCompleted.toLocaleString());
 
         var n_projects = Math.floor(data['n_projects']/10)*10;
-        $("#n_projects").html(n_projects);
+        $("#n_projects").html(n_projects.toLocaleString());
     }
 });
 

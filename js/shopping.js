b0ec678a46acf4def73c590ae5040315aa8f23af
Daniel Lombraña González <teleyinex@gmail.com>
2016-07-15 14:07:01 +0200

    Shopping cart methods and code.

--- js/shopping.js
+++ js/shopping.js
@@ -1,34 +1,66 @@
-$("#newclient").modal("show");
+var invoice;
+
+$(".btn-shoppingcart").off('click').on('click', function(evt){
+    evt.preventDefault();
+    invoice = $(this).data();
+    $("#newclient").modal("show");
+    var text = "Buy " + invoice['notes'] + " for " + invoice['cost'] + "€";
+    $("#product").text(text);
+    $("#notes").text(invoice['notes']);
+});
 
 
 
 $("#checkout").off('click').on('click', function(evt){
     evt.preventDefault();
+    createClient();
 
+});
+
+
+function createClient() {
     $.ajax({
-        url: "http://localhost:5000/shopping",
+        url: "http://localhost:5000/newclient",
         crossDomain: true,
         xhrFields: { withCredentials: true }
     }).done(function(data) {
-        console.log(data); 
         $("#csrf_token").attr("value", data['csrf_token']);
 
         var form = $("#formNewClient").serialize();
-        console.log("HOLA FORM");
-        console.log(form);
-
 
         var csrftoken = data['csrf_token'];
-
         
         var xhr = $.ajax({
           type: "POST",
-          url: "http://localhost:5000/shopping",
+          url: "http://localhost:5000/newclient",
           data: form,
           dataType: "json",
           crossDomain: true,
           xhrFields: { withCredentials: true }
-        }).done(function(datapost){ console.log(datapost);});
+        }).done(createInvoice);
 
     });
-});
+}
+
+function createInvoice(client) {
+    $.ajax({
+        url: "http://localhost:5000/newinvoice",
+        crossDomain: true,
+        xhrFields: { withCredentials: true }
+    }).done(function(data) {
+
+        console.log(client);
+        invoice['csrf_token'] = data['csrf_token'];
+        invoice['client_id'] = client.data['id'];
+        console.log(invoice);
+
+        var xhr = $.ajax({
+          type: "POST",
+          url: "http://localhost:5000/newinvoice",
+          data: invoice,
+          dataType: "json",
+          crossDomain: true,
+          xhrFields: { withCredentials: true }
+        }).done(function(datapost){ console.log("Invoice created!"); console.log(datapost);});
+    });
+}

1f6b73401ae7b47d8a1ade2252c313d2577e35f8
Daniel Lombraña González <teleyinex@gmail.com>
2016-11-07 17:21:58 +0100

    Remove function as not HTTPS

--- js/case-studies/micropasts/data.js
+++ js/case-studies/micropasts/data.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
-    var url = "http://crowdsourced.micropasts.org/api/globalstats";
-    
-    $.ajax({
-      dataType: "json",
-      url: url,
-      success: success
-    });
+    //var url = "http://crowdsourced.micropasts.org/api/globalstats";
+    //
+    //$.ajax({
+    //  dataType: "json",
+    //  url: url,
+    //  success: success
+    //});
     
     function success(data){
         var n_users = Math.floor(data['n_users']/100)*100;

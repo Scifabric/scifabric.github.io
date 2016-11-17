7122955648e3dabee6904d08c8c53bf6f30946fc
Daniel Lombraña González <teleyinex@gmail.com>
2016-11-17 17:12:03 +0100

    Fixes for mobile.

--- assets/js/success-stories/cruk/animprocess.js
+++ assets/js/success-stories/cruk/animprocess.js
@@ -1,9 +1,29 @@
-var vivusProcess = new Vivus('my-svg', {duration: 200});
+var lg = true;
+if (window.matchMedia("(max-width: 420px)").matches) {
+    lg = false;
+}
 
-vivusProcess.stop();
+var vivusProcess = [];
+
+if (lg) {
+    var tmp = new Vivus('my-svg', {duration: 200});
+    vivusProcess.push(tmp)
+    vivusProcess[0].stop();
+}
+else {
+    var tmp = new Vivus('process-volunteers', {duration: 200});
+    vivusProcess.push(tmp)
+    var tmp = new Vivus('process-experts', {duration: 200});
+    vivusProcess.push(tmp)
+    vivusProcess[0].stop();
+    vivusProcess[1].stop();
+}
+
 
 function startVivus() {
-    vivusProcess.play();
+    for(var i=0;i<vivusProcess.length;i++) {
+        vivusProcess[i].play();
+    }
 }
 var waypointProcess = new Waypoint({
     element: document.getElementById('process-waypoint'),

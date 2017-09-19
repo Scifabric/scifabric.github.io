5d311c6fbd54f023f5e801de478cea9cc3d67d7c
Daniel Lombraña González <teleyinex@gmail.com>
2017-09-19 11:51:10 +0200

    Fixes.

--- assets/js/pricing/components/contact.vue
+++ assets/js/pricing/components/contact.vue
@@ -5,31 +5,31 @@
                     <h2 class="subtitle is-6">Drop us a line at <a href="mailto:info@scifabric.com">info@scifabric.com</a> or fill in this form</h2>
                     <div class="has-text-left">
                         <b-field label="What it is first name?">
-                            <b-input v-model="first_name"></b-input>
+                            <b-input v-model="formData.first_name"></b-input>
                         </b-field>
                         <b-field label="Your last name?">
-                            <b-input v-model="last_name"></b-input>
+                            <b-input v-model="formData.last_name"></b-input>
                         </b-field>
                         <b-field label="And your email?">
                             <b-input type="email"
                                      value="john@"
                                      maxlength="30"
-                                     v-model="email">
+                                     v-model="formData.email">
                             </b-input>
                         </b-field>
                         <b-field label="Drop us a line">
-                            <b-input v-model="message" type="textarea"
+                            <b-input v-model="formData.message" type="textarea"
                                                        minlength="10"
                                                        maxlength="100"
                                                        placeholder="">
                             </b-input>
                         </b-field>
                         <div class="field">
-                            <b-checkbox v-model="subscribed">Keep me informed of scifabric’s news (no spam. We promise!)
+                            <b-checkbox v-model="formData.subscribe">Keep me informed of scifabric’s news (no spam. We promise!)
                             </b-checkbox>
                         </div>
                     </div>
-                    <p class="button is-medium is-primary">Send</p>
+                    <p @click="sendMail" class="button is-medium is-primary">Send</p>
                     <!--<p class="title is-6 has-text-primary">Scifabric is Madrid and London based</p>-->
         </div>
         <div v-else class="step2">
@@ -46,19 +46,46 @@
     </b-modal>
 </template>
 <script>
+import axios from 'axios';
+import $ from 'jquery';
 export default {
     props: ['modalactive'],
     data() {
         return {
-            first_name: '',
-            last_name: '',
-            email: '',
-            message: '', 
-            subscribed: false,
+			formData: {
+            	first_name: '',
+            	last_name: '',
+            	email: '',
+            	message: '', 
+            	subscribe: false,
+				subject: 'HOLA',
+			},
             step: 1 
         }
               
-    }
+    },
+	methods: {
+		sendMail(){
+			axios.defaults.withCredentials = true
+			axios.defaults.crossDomain = true
+        	//var url = "https://formchimp.scifabric.com/register"
+        	var url = "http://localhost:5000/register"
+            var self = this
+			axios.get(url)
+				.then(response => {
+					console.log(response.data)	
+					var csrftoken = response.data['csrf_token']
+					axios.post(url, self.formData, 
+							   {headers: {'X-CSRFToken': csrftoken, 'Content-Type': 'application/x-www-form-urlencoded'}})
+						.then(response => {
+								console.log("Done!")
+								//self.step = 2
+						})
+					    .catch(error => {console.log(error)})
+				})
+			    .catch(error => {console.log(error)})	
+			}
+	}
 }
 </script>
 <style>
